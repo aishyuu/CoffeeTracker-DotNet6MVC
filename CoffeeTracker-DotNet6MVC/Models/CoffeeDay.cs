37be54cde402e11029ffe3caef50100b565e5f1e
aishyuu <ivanmatias1331@gmail.com>
2024-07-25 14:00:22 -0700

    Add project files.

---
+++ CoffeeTracker-DotNet6MVC/Models/CoffeeDay.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeTracker_DotNet6MVC.Models
+{
+    public class CoffeeDay
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime Date { get; set; } = DateTime.Today;
+        [Required]
+        [DisplayName("Cups Drank")]
+        public int CupsDrank { get; set; }
+    }
+}
