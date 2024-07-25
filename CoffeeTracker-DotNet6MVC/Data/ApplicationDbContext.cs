628c7187b734eb93d495622b137ec87cd6247e9d
aishyuu <ivanmatias1331@gmail.com>
2024-07-25 14:10:52 -0700

    Created ApplicationDbContext

---
+++ CoffeeTracker-DotNet6MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeTracker_DotNet6MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeTracker_DotNet6MVC.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<CoffeeDay> CoffeeDays { get; set; }
+    }
+}
